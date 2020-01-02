ab596fdd53fcfc5100076cbdf234dfccd45ec55e
Lianne Van Westrhenen <lianne.vanwestrhenen@incentro.com>
2020-01-02 16:42:48 +0100

    add board

---
+++ src/components/square.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Square extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <button className="square">
+                {this.props.value}
+            </button>
+        )
+    }
+}
+
+export default Square;
